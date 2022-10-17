2b762deb070b598ae66cca2f2a750056c691fc0c
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-10-17 17:14:48 -0300

    feat: add requisito 03 - mais vendido

---
+++ challenges/desafio3.js
@@ -0,0 +1,9 @@
+db.produtos.find({
+
+}, {
+  _id: 0,
+  nome: 1,
+  vendidos: 1,
+}).sort({ vendidos: -1 }).limit(1);
+
+// https://stackoverflow.com/questions/32076382/mongodb-how-to-get-max-value-from-collections
