1c87fce290b7d256e0e48348b156c64b0427c24d
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-10-19 09:14:30 -0300

    feat: add requisito 9 - qnt estava escrito errado AAAAAAAAAAAAAAAAAA

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,9 +1,9 @@
-// db.produtos.find({
-//   valoresNutricionais: { $elemMatch: {
-//     tipo: "calorias",
-//     quantidae: { $lt: 500 },
-//   } },
-// }, {
-//   _id: 0,
-//   nome: 1,
-// });
+db.produtos.find({
+  valoresNutricionais: { $elemMatch: {
+    tipo: "calorias",
+    quantidade: { $lt: 500 },
+  } },
+}, {
+  _id: 0,
+  nome: 1,
+});
